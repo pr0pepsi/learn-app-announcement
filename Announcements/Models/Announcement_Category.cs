1f594eba1a0145ded4404efab716933b2584e863
mateusz_urbas <mati.u180@gmail.com>
2017-04-27 21:44:34 +0200

    first commit

---
+++ Announcements/Models/Announcement_Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Announcements.Models
+{
+    public class Announcement_Category
+    {
+        public Announcement_Category()
+        {
+         
+        }
+
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int AnnouncementId { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual Announcement Announcement { get; set; }
+
+    }
+}
