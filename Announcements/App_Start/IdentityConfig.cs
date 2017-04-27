302b3884fd0ffd29b319d52ecfcfb11770fb3cf7
mateusz_urbas <mati.u180@gmail.com>
2017-04-27 22:50:11 +0200

    add context

--- Announcements/App_Start/IdentityConfig.cs
+++ Announcements/App_Start/IdentityConfig.cs
@@ -42,7 +42,7 @@ namespace Announcements
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
         {
-            var manager = new ApplicationUserManager(new UserStore<User>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(new UserStore<User>(context.Get<AnnouncementContext>()));
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<User>(manager)
             {
