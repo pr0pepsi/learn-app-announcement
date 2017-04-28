0090819caeb6ca33d71e39748218c62e02154c63
mateusz_urbas <mati.u180@gmail.com>
2017-04-28 18:31:30 +0200

    Add announcement controller

---
+++ Announcements/Controllers/AnnouncementController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Repository.Models;
+
+namespace Announcements.Controllers
+{
+    public class AnnouncementController : Controller
+    {
+        private AnnouncementContext db = new AnnouncementContext();
+
+        // GET: Announcement
+        public ActionResult Index()
+        {
+            var announcements = db.Announcements.Include(a => a.User);
+            return View(announcements.ToList());
+        }
+
+        // GET: Announcement/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Announcement announcement = db.Announcements.Find(id);
+            if (announcement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(announcement);
+        }
+
+        // GET: Announcement/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Announcement/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,TextContent,Title,AddDateTime,UserId")] Announcement announcement)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Announcements.Add(announcement);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", announcement.UserId);
+            return View(announcement);
+        }
+
+        // GET: Announcement/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Announcement announcement = db.Announcements.Find(id);
+            if (announcement == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", announcement.UserId);
+            return View(announcement);
+        }
+
+        // POST: Announcement/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,TextContent,Title,AddDateTime,UserId")] Announcement announcement)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(announcement).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", announcement.UserId);
+            return View(announcement);
+        }
+
+        // GET: Announcement/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Announcement announcement = db.Announcements.Find(id);
+            if (announcement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(announcement);
+        }
+
+        // POST: Announcement/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Announcement announcement = db.Announcements.Find(id);
+            db.Announcements.Remove(announcement);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
